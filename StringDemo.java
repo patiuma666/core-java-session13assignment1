7ac93ac29ae78994aa2c8248ed645e25646f6edb
patiuma666 <patiuma666@gmail.com>
2017-07-31 13:36:47 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,18 @@
+package assignment13;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int integer = 250;
+		
+		String string = String.valueOf(integer);
+		
+		string = Integer.toString(integer);
+		
+		System.out.println("After converting the integer variable to String ");
+		System.out.println("it gives the output: " +integer);
+
+	}
+
+}
